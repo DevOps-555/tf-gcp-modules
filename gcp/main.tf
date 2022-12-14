9c9afbb96dfb7ef4356d608729991f491ac3ff9a
Vivek <vivek@gmail.com>
2022-12-15 00:08:16 +0530

    Changed Path

--- gcp/main.tf
+++ gcp/main.tf
@@ -12,5 +12,14 @@ module "ansible-server-01" {
   source = "./modules/gce"
   gcp_project  = var.gcp_project
 
-  gce-instance-01 = var.gce-instance-01
+  gce_instance_name       = var.gce_instance_name
+  gce_hostname            = var.gce_hostname
+  gce_region              = var.gce_region
+  gce_zones               = var.gce_zones
+  gce_tags                = ["ansible", "ansible-server-01"]
+  gce_machine_type        = var.gce_machine_type
+  gce_boot_disk_size      = var.gce_boot_disk_size
+  gce_boot_disk_image     = var.gce_boot_disk_image
+  gce_vpc_nw              = "default"
+  gce_snw                 = "default"
 }
