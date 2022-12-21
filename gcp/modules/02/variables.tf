11400b876a7b741fd31b0226dfd53c30c14f5230
vivek reddy <62931563+vivekreddy0808@users.noreply.github.com>
2022-12-21 20:35:28 +0530

    New Instance

---
+++ gcp/modules/02/variables.tf
@@ -0,0 +1,19 @@
+## --- DEFAULT PARAMETERS ------------------------------------------------------------------------------------------------
+
+variable "gcp_project" {}
+
+## --- INSTANCE 02 PARAMETERS ---------------------------------------------------------------------------------------------
+variable "gce-ansible-node-01" {
+    type = object({
+            gce_instance_name   = string
+            gce_hostname        = string
+            gce_region          = string
+            gce_zones           = string
+            gce_tags            = list(string)
+            gce_machine_type    = string
+            gce_boot_disk_size  = number
+            gce_boot_disk_image = string
+            gce_vpc_nw          = string
+            gce_snw             = string
+    })
+}
