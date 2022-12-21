41d4946f4f74af4e046a8ee3531c4afcc1c38d6b
vivek reddy <62931563+vivekreddy0808@users.noreply.github.com>
2022-12-21 20:18:06 +0530

    Added Nee Instance

---
+++ gcp/modules/gce/ansible-node-intance-01.tf
@@ -0,0 +1,33 @@
+resource "google_compute_instance" "test-instance" {
+  project      = var.gcp_project
+  name         = var.gce-instance-03.gce_instance_name
+  hostname     = var.gce-instance-03.gce_hostname
+  machine_type = var.gce-instance-03.gce_machine_type
+  zone         = var.gce-instance-03.gce_zones
+  tags         = ["ansible-server"]
+  boot_disk {
+    initialize_params {
+      image = var.gce-instance-03.gce_boot_disk_image
+      size  = var.gce-instance-03.gce_boot_disk_size
+      labels = {
+        name = "ansible-server"
+        sever = "remote-server"
+      }
+    }
+  }
+
+  network_interface {
+    network = var.gce-instance-03.gce_vpc_nw
+    subnetwork = var.gce-instance-03.gce_snw
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+}
