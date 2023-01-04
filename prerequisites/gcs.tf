2e060af24405c44dbd443552d09d88816ef267d2
vivek reddy <62931563+vivekreddy0808@users.noreply.github.com>
2023-01-04 22:14:24 +0530

    Updated Versioning

--- prerequisites/gcs.tf
+++ prerequisites/gcs.tf
@@ -1,21 +1,10 @@
 
 resource "google_storage_bucket" "default" {
   name = var.bucket_name
+  force_destory = false
   storage_class = var.storage_class
   location = var.bucket_location
+  versioning {
+    enabled = true
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-# bucket = "terraform-state-bucket-001"
-# prefix = "gce/"
